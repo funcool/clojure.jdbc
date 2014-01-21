20b8a2c261f075d46599f0f6cadfb320347c0502
Andrey Antukh <niwi@niwi.be>
2014-01-21 20:29:41 +0100

    Refresh project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject be.niwi/clj.jdbc "0.1.0-beta4"
+(defproject be.niwi/clj.jdbc "0.1.0-beta5"
   :description "Alternative implementation of jdbc wrapper for clojure.
 
 For this momment, the official wrapper `clojure.jdbc` has very unclear
@@ -11,17 +11,13 @@ official library."
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [com.mchange/c3p0 "0.9.5-pre5"]]}
              :test {:dependencies [[com.h2database/h2 "1.3.170"]
-                                   [postgresql "9.1-901.jdbc4"]
-                                   [com.mchange/c3p0 "0.9.5-pre5"]]}}
-
-  ;; :main ^:skip-aot jdbc.core
+                                   [postgresql "9.1-901.jdbc4"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.mchange/c3p0 "0.9.5-pre6"]]
   :plugins [[lein-marginalia "0.7.1"]
             [codox "0.6.6"]]
   :codox {:output-dir "docs/codox"
           :src-dir-uri "http://github.com/niwibe/clj.jdbc/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :aot [jdbc.types.connection jdbc.types.resultset]
   :target-path "target/%s")
