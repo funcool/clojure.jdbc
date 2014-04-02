ec3361ed57c32129e6e41be5b4b773650e65f73e
Andrey Antukh <niwi@niwi.be>
2014-04-02 21:57:00 +0200

    Fix project url of c3p0 module.

--- modules/c3p0/project.clj
+++ modules/c3p0/project.clj
@@ -1,7 +1,7 @@
 (defproject be.niwi/clj.jdbc-c3p0 "0.1.1"
   :description "clj.jdbc extension for c3p0 (a mature, highly concurrent JDBC
 Connection pooling library)."
-  :url "http://github.com/niwibe/clj.jdbc-c3p0"
+  :url "http://github.com/niwibe/clj.jdbc"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
