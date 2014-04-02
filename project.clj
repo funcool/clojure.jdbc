72c3c4b4d2a26d21e170e2a9398c1190bb6e6b8f
Andrey Antukh <niwi@niwi.be>
2014-04-02 21:19:55 +0200

    Remove a connection pool dependency from main project...
    and add it as a separate module.

--- project.clj
+++ project.clj
@@ -9,20 +9,21 @@ official library."
   :url "http://github.com/niwibe/clj.jdbc"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:bench {:source-paths ["bench/"]
-                     :main jdbc.bench
-                     :dependencies [[org.clojure/java.jdbc "0.3.3"]
-                                    [postgresql "9.1-901.jdbc4"]]}
-             :test {:dependencies [[com.h2database/h2 "1.3.170"]
-                                   [postgresql "9.1-901.jdbc4"]]}}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.mchange/c3p0 "0.9.5-pre6"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :aot [jdbc.types.connection
         jdbc.types.resultset]
   :plugins [[lein-marginalia "0.7.1"]
-            [codox "0.6.6"]]
+            [codox "0.6.7"]
+            [lein-sub "0.3.0"]]
   :codox {:output-dir "doc/api"
           :src-dir-uri "http://github.com/niwibe/clj.jdbc/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :target-path "target/%s")
+  :sub ["modules/c3p0"]
+  :profiles {:bench {:source-paths ["bench/"]
+                     :main jdbc.bench
+                     :dependencies [[org.clojure/java.jdbc "0.3.3"]
+                                    [postgresql "9.3-1101.jdbc41"]]}
+             :test {:dependencies [[com.h2database/h2 "1.3.170"]
+                                   [postgresql "9.3-1101.jdbc41"]
+                                   [com.mchange/c3p0 "0.9.5-pre7"]]
+                    :source-paths ["modules/c3p0/src"]}})
