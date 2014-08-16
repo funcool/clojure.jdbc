a907192c3460c77b9c3b9f22e755e6d7a916c7fc
Andrey Antukh <niwi@niwi.be>
2014-08-16 17:39:48 +0200

    Move metadata utils functions to jdbc.meta namespace.

--- src/jdbc/meta.clj
+++ src/jdbc/meta.clj
@@ -1,4 +1,4 @@
-;; Copyright 2013 Andrey Antukh <niwi@niwi.be>
+;; Copyright 2014 Andrey Antukh <niwi@niwi.be>
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License")
 ;; you may not use this file except in compliance with the License.
@@ -12,111 +12,100 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(ns jdbc.types.connection)
-
-(def ^:private isolation-level-map
-  {:none nil
-   :read-commited (java.sql.Connection/TRANSACTION_READ_UNCOMMITTED)
-   :repeatable-read (java.sql.Connection/TRANSACTION_REPEATABLE_READ)
-   :serializable (java.sql.Connection/TRANSACTION_SERIALIZABLE)})
-
-(defrecord Connection [connection metadata]
-  java.io.Closeable
-  (close [this]
-    (.close (:connection this))))
-
-(defn is-connection?
-  "Test if a value is a connection instance."
-  [^Connection c]
-  (instance? Connection c))
+(ns jdbc.meta
+  "Connection metadata access methods."
+  (:require [jdbc.types :as types])
+  (:import jdbc.types.Connection))
 
 (defn vendor-name
   "Get connection vendor name."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDatabaseProductName (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDatabaseProductName meta)))
 
 (defn catalog-name
   "Given a connection, get a catalog name."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getCatalog (:connection c)))
+  (let [^java.sql.Connection conn (:connection c)]
+    (.getCatalog conn)))
 
 (defn schema-name
   "Given a connection, get a schema name."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getSchema (:connection c)))
+  (let [^java.sql.Connection conn (:connection c)]
+    (.getSchema conn)))
 
 (defn is-readonly?
   "Returns true if a current connection is
   in read-only model."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.isReadOnly (:connection c)))
+  (let [^java.sql.Connection conn (:connection c)]
+    (.isReadOnly conn)))
 
 (defn is-valid?
   "Given a connection, return true if connection
   has not ben closed it still valid."
-  [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.isValid (:connection c)))
+  ([^Connection c]
+     (is-valid? c 0))
+  ([^Connection c ^long timeout]
+     (let [^java.sql.Connection conn (:connection c)]
+       (.isValid conn timeout))))
 
 (defn network-timeout
   "Given a connection, get network timeout."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  ( .getNetworkTimeout (:connection c)))
+  (let [^java.sql.Connection conn (:connection c)]
+    (.getNetworkTimeout conn)))
 
 (defn isolation-level
   "Given a connection, get a current isolation level."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (let [ilvalue (.getTransactionIsolation (:connection c))]
-    (cond
-      (= ilvalue java.sql.Connection/TRANSACTION_READ_UNCOMMITTED) :read-commited
-      (= ilvalue java.sql.Connection/TRANSACTION_REPEATABLE_READ) :repeatable-read
-      (= ilvalue java.sql.Connection/TRANSACTION_SERIALIZABLE) :serializable
-      :else :none)))
+  (let [^java.sql.Connection conn (:connection c)
+        ilvalue (.getTransactionIsolation conn)]
+    (condp = ilvalue
+      java.sql.Connection/TRANSACTION_READ_UNCOMMITTED :read-commited
+      java.sql.Connection/TRANSACTION_REPEATABLE_READ  :repeatable-read
+      java.sql.Connection/TRANSACTION_SERIALIZABLE     :serializable
+      :none)))
 
 (defn db-major-version
   "Given a connection, return a database major
   version number."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDatabaseMajorVersion (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDatabaseMajorVersion meta)))
 
 (defn db-minor-version
   "Given a connection, return a database minor
   version number."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDatabaseMinorVersion (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDatabaseMinorVersion meta)))
 
 (defn db-product-name
   "Given a connection, return a database product
   name from it metadata."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDatabaseProductName (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDatabaseProductName meta)))
 
 (defn db-product-version
   "Given a connection, return a database product
   version from it metadata."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDatabaseProductVersion (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDatabaseProductVersion meta)))
 
 (defn driver-name
   "Given a connection, return a current driver name
   used for this connection."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDriverName (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDriverName meta)))
 
 (defn driver-version
   "Given a connection, return a current driver version
   used for this connection."
   [^Connection c]
-  {:pre [(is-connection? c)]}
-  (.getDriverVersion (:metadata c)))
+  (let [^java.sql.DatabaseMetaData meta (:metadata c)]
+    (.getDriverVersion meta)))
