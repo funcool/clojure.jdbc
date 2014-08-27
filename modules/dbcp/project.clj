04dd83117324ddce4f930c3da05d4b90f75bb23a
Andrey Antukh <niwi@niwi.be>
2014-08-27 11:09:27 +0200

    Remove dbcp from codebase (moved to separated repository)

--- modules/dbcp/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject clojure.jdbc/clojure.jdbc-dbcp "0.2.0"
-  :description "clojure.jdbc extension for apache commonds dbcp jdbc
-connection pool implementation."
-  :url "http://github.com/niwibe/clojure.jdbc"
-  :license {:name "Apache 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.commons/commons-dbcp2 "2.0.1"]])
