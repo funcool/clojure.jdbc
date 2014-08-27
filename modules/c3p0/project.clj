5593d42654ac37c7943b0b146f809ebfb1501732
Andrey Antukh <niwi@niwi.be>
2014-08-27 23:06:50 +0200

    Remove c3p0 code from codebase.

--- modules/c3p0/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.2.0"
-  :description "clojure.jdbc extension for c3p0 (a mature, highly concurrent JDBC
-Connection pooling library)."
-  :url "http://github.com/niwibe/clojure.jdbc"
-  :license {:name "Apache 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.mchange/c3p0 "0.9.5-pre8"]])
