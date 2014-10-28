42d1363ab4f06a3a1d3927bedd774d39963c2f7a
Andrey Antukh <niwi@niwi.be>
2014-10-28 13:43:49 +0100

    Minor version updates on project.clj

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [potemkin "0.3.4" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-marginalia "0.7.1"]
-            [codox "0.8.9"]]
+            [codox "0.8.10"]]
   :codox {:output-dir "doc/api"
           :src-dir-uri "http://github.com/niwibe/clojure.jdbc/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:bench {:source-paths ["bench/"]
                      :main jdbc.bench
                      :dependencies [[org.clojure/java.jdbc "0.3.5"]
-                                    [postgresql "9.3-1101.jdbc41"]]}
+                                    [postgresql "9.3-1102.jdbc41"]]}
              :test {:dependencies [[com.h2database/h2 "1.3.176"]
-                                   [postgresql "9.3-1101.jdbc41"]
+                                   [postgresql "9.3-1102.jdbc41"]
                                    [cheshire "5.3.1"]]}})
