75389b0de6c54ca5b48679a6c78d9378794c5dbf
Andrey Antukh <niwi@niwi.be>
2014-10-28 19:27:20 +0100

    Update build docs script.

--- build-docs.sh
+++ build-docs.sh
@@ -2,17 +2,14 @@
 lein doc
 (cd doc; make)
 cp -vr doc/index.html /tmp/index.html;
-cp -vr doc/static /tmp/static
 mv -v doc/api /tmp/api
 git checkout gh-pages;
 
 rm -rf static index.html api
-mv -fv /tmp/static .
 mv -fv /tmp/index.html .
 mv -fv /tmp/api .
 
 git add --all index.html
 git add --all api
-git add --all static
 
 git commit -a -m "Update doc"
