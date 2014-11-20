d5490af2835cafef6e0cadbc581489cb995e4933
Andrey Antukh <niwi@niwi.be>
2014-11-20 22:51:52 +0100

    Set default javac target to java7 and increment version to 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure.jdbc "0.3.1"
+(defproject clojure.jdbc "0.3.2"
   :description "clojure.jdbc is a library for low level, jdbc-based database access."
   :url "http://github.com/niwibe/clojure.jdbc"
   :license {:name "Apache 2.0"
@@ -7,6 +7,7 @@
                  [potemkin "0.3.4" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-marginalia "0.7.1"]
             [codox "0.8.10"]]
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :codox {:output-dir "doc/api"
           :src-dir-uri "http://github.com/niwibe/clojure.jdbc/blob/master/"
           :src-linenum-anchor-prefix "L"}
