6aaf6061392caffcc4759e6385c32697be98f954
Andrey Antukh <niwi@niwi.be>
2015-01-11 11:36:18 +0100

    Update build script.

--- build-docs.sh
+++ build-docs.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
+VERSION="latest"
+
 lein doc
 (cd doc; make)
-cp -vr doc/index.html /tmp/index.html;
-mv -v doc/api /tmp/api
+
+rm -rf /tmp/index.html /tmp/api
+
+mv doc/index.html /tmp/index.html;
+mv doc/api /tmp/api
+
 git checkout gh-pages;
 
-rm -rf static index.html api
-mv -fv /tmp/index.html .
-mv -fv /tmp/api .
+rm -rf ./index.html
+rm -rf ./api
+
+mv -fv /tmp/index.html ./
+mv -fv /tmp/api ./
 
 git add --all index.html
 git add --all api
+git commit -a -m "Update ${VERSION} doc"
 
-git commit -a -m "Update doc"
