72caa26fa09d9d660050c4a32ef133d19c5fa2d5
Andrey Antukh <niwi@niwi.be>
2015-03-15 00:14:29 +0100

    Move resultset ns out of utils.

--- src/jdbc/resultset.clj
+++ src/jdbc/resultset.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(ns jdbc.util.resultset
+(ns jdbc.resultset
   "ResultSet conversion functions."
   (:require [clojure.string :as str]
             [jdbc.proto :as proto])
