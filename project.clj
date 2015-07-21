a4c1b68e817d6061d94ea9f4b03cd1f381efd67a
Andrey Antukh <niwi@niwi.nz>
2015-07-22 01:00:22 +0300

    Update all dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://github.com/niwibe/clojure.jdbc"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
-                 [potemkin "0.3.13"]]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [potemkin "0.4.1"]]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.4.187"]
                                   [postgresql "9.3-1102.jdbc41"]
-                                  [hikari-cp "1.2.3" :exclusions [com.zaxxer/HikariCP]]
-                                  [com.zaxxer/HikariCP-java6 "2.3.7"]
-                                  [cheshire "5.4.0"]]
+                                  [hikari-cp "1.2.4" :exclusions [com.zaxxer/HikariCP]]
+                                  [com.zaxxer/HikariCP-java6 "2.3.9"]
+                                  [cheshire "5.5.0"]]
                    :codeina {:sources ["src"]
                              :exclude [jdbc.core-deprecated
                                        jdbc.impl
@@ -27,7 +27,7 @@
                      :main jdbc.bench
                      :global-vars {*warn-on-reflection* true
                                    *unchecked-math* :warn-on-boxed}
-                     :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                                    [org.clojure/java.jdbc "0.3.6"]
+                     :dependencies [[org.clojure/clojure "1.7.0"]
+                                    [org.clojure/java.jdbc "0.3.9"]
                                     [com.h2database/h2 "1.4.187"]
                                     [criterium "0.4.3"]]}})
