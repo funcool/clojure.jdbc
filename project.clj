daa53520c9abc1e384a360c19d60a8a3304e20d7
Andrey Antukh <niwi@niwi.nz>
2016-04-29 17:48:16 +0300

    Set version to 0.8.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/clojure.jdbc "0.7.0"
+(defproject funcool/clojure.jdbc "0.8.0"
   :description "clojure.jdbc is a library for low level, jdbc-based database access."
   :url "http://github.com/niwibe/clojure.jdbc"
   :license {:name "Apache 2.0"
@@ -10,7 +10,7 @@
    {:dependencies [[com.h2database/h2 "1.4.191"]
                    [org.postgresql/postgresql "9.4.1208.jre7"]
                    [hikari-cp "1.6.1"]
-                   [cheshire "5.5.0"]]
+                   [cheshire "5.6.1"]]
     :codeina {:sources ["src"]
               :exclude [jdbc.impl
                         jdbc.transaction
@@ -19,13 +19,12 @@
               :target "doc/dist/latest/api"
               :src-uri "http://github.com/niwibe/clojure.jdbc/blob/master/"
               :src-uri-prefix "#L"}
-    :plugins [[lein-ancient "0.6.7"]
-              [funcool/codeina "0.3.0"]]}
+    :plugins [[lein-ancient "0.6.10"]
+              [funcool/codeina "0.4.0"]]}
    :bench {:source-paths ["bench/"]
            :main jdbc.bench
            :global-vars {*warn-on-reflection* true
                          *unchecked-math* :warn-on-boxed}
-           :dependencies [[org.clojure/clojure "1.7.0"]
-                          [org.clojure/java.jdbc "0.4.2"]
+           :dependencies [[org.clojure/java.jdbc "0.5.8"]
                           [com.h2database/h2 "1.4.191"]
                           [criterium "0.4.4"]]}})
